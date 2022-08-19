206cf46cd568804c7112e424ca025fdc40f10307
tom-jus <101270851+tom-jus@users.noreply.github.com>
2022-08-19 18:15:48 -0300

    Add files via upload

---
+++ 01-ejercicio.js
@@ -0,0 +1,17 @@
+
+const my_list = [
+    "Tomas", 
+    23, 
+    true, 
+    new Date("january 31 1999")
+]
+const book_fav = {
+        title: "el poder del ahora",
+        author: "eckhart tolle",
+        date: 1997,
+        url: "megustaleer.com.ar"
+    }
+
+
+console.log(my_list);
+console.log(book_fav);
